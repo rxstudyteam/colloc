89b3aca27d7f2a62af63e1f23ca55963cb158718
noah.kim <babosamo@gmail.com>
2018-07-12 00:28:38 +0900

    class -> data class

--- app/src/main/java/com/karrel/colloc/model/airdata/AirData.kt
+++ app/src/main/java/com/karrel/colloc/model/airdata/AirData.kt
@@ -1,13 +1,10 @@
 package com.karrel.colloc.model.airdata
 
-import java.util.ArrayList
-
-class AirData {
-    var overallValue = OverallValue()
-    var currentValues: List<CurrentValue> = ArrayList()
-    var hourlyForecasts: List<HourlyForecast> = ArrayList()
-    var dailyForecasts: List<DailyForecast> = ArrayList()
-    var extraInformations: List<ExtraInformation> = ArrayList()
-    var note = "콜록콜록은 사용자분과 가장 가까이 위치한, 정상 작동 중인 측정소의 실시간 정보를 보여드립니다.\n\n본 자료는 한국환경공단(에어코리아)과 기상청에서 제공하는 실시간 관측 자료이며 실제 대기농도 수치와 다를 수 있습니다."
-
-}
+data class AirData(
+        var overallValue: OverallValue = OverallValue(),
+        var currentValues: MutableList<CurrentValue> = mutableListOf(),
+        var hourlyForecasts: MutableList<HourlyForecast> = mutableListOf(),
+        var dailyForecasts: MutableList<DailyForecast> = mutableListOf(),
+        var extraInformation: MutableList<ExtraInformation> = mutableListOf(),
+        var note: String = "콜록콜록은 사용자분과 가장 가까이 위치한, 정상 작동 중인 측정소의 실시간 정보를 보여드립니다.\n\n본 자료는 한국환경공단(에어코리아)과 기상청에서 제공하는 실시간 관측 자료이며 실제 대기농도 수치와 다를 수 있습니다."
+)
