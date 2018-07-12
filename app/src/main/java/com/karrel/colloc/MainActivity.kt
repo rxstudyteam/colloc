39591ab84a79d1433c04dd0f66c9932433d8d851
windwishers <shadowog@gmail.com>
2018-07-12 22:34:17 +0900

    Crashlytics 추가설정. 기본 파이어베이스를 설정고. gms 까지 설정 되어 있어야 리포트가 올라간다.

--- app/src/main/java/com/karrel/colloc/MainActivity.kt
+++ app/src/main/java/com/karrel/colloc/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.karrel.colloc
 
 import android.os.Bundle
+import android.view.View
 import com.karrel.colloc.base.BaseActivity
+import android.view.ViewGroup
+import android.widget.Button
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+
+
+
 
 class MainActivity : BaseActivity() {
 
@@ -13,6 +22,24 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // do something
+
+//        val fabric = Fabric.Builder(this)
+//                .kits(Crashlytics())
+//                .debuggable(true)           // Enables Crashlytics debugger
+//                .build()
+//        Fabric.with(fabric)
+
+        val crashButton = Button(this)
+        crashButton.setText("Crash!")
+        crashButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                Crashlytics.getInstance().crash() // Force a crash
+            }
+        })
+        addContentView(crashButton,
+                ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT))
+
     }
 
     override val initView: () -> Unit = {
