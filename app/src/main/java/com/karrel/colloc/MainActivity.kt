16d04bea77be20758136bc5c64af16d9630fd53e
windwishers <shadowog@gmail.com>
2018-07-12 23:26:40 +0900

    페브릭 설정몇 테스트 완료.

--- app/src/main/java/com/karrel/colloc/MainActivity.kt
+++ app/src/main/java/com/karrel/colloc/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.karrel.colloc
 
 import android.os.Bundle
+import android.view.View
 import com.karrel.colloc.base.BaseActivity
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+
 
 class MainActivity : BaseActivity() {
 
@@ -13,9 +18,29 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // do something
+        Fabric.with(this, Crashlytics())
+
+        // TODO: Move this to where you establish a user session
+        logUser();
+
+
     }
 
     override val initView: () -> Unit = {
 
     }
+
+    private fun logUser() {
+        // TODO: Use the current user's information
+        // You can call any combination of these three methods
+        Crashlytics.setUserIdentifier("12345")
+        Crashlytics.setUserEmail("user@fabric.io")
+        Crashlytics.setUserName("Test User")
+    }
+
+
+    fun forceCrash(view: View) {
+        throw RuntimeException("This is a crash")
+    }
+
 }
