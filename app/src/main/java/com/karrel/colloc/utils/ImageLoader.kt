d3be0f7553084aa6ab091808b19c3d7a78cd1274
noah.kim <babosamo@gmail.com>
2018-07-19 00:14:10 +0900

    remove transformations

--- app/src/main/java/com/karrel/colloc/utils/ImageLoader.kt
+++ app/src/main/java/com/karrel/colloc/utils/ImageLoader.kt
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment
 import android.widget.ImageView
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
-import jp.wasabeef.glide.transformations.BlurTransformation
 
 
 object ImageLoader {
@@ -23,29 +22,16 @@ object ImageLoader {
         loadImage(GlideApp.with(fragment), targetView, imageUrl, isNoCache)
     }
 
-    fun loadCircleImage(context: Context, targetView: ImageView, imageUrl: String) {
-        loadCircleImage(GlideApp.with(context), targetView, imageUrl)
+    fun loadCircleImage(context: Context, targetView: ImageView, imageUrl: String, isNoCache: Boolean = false) {
+        loadCircleImage(GlideApp.with(context), targetView, imageUrl, isNoCache)
     }
 
-    fun loadCircleImage(activity: Activity, targetView: ImageView, imageUrl: String) {
-        loadCircleImage(GlideApp.with(activity), targetView, imageUrl)
+    fun loadCircleImage(activity: Activity, targetView: ImageView, imageUrl: String, isNoCache: Boolean = false) {
+        loadCircleImage(GlideApp.with(activity), targetView, imageUrl, isNoCache)
     }
 
-    fun loadCircleImage(fragment: Fragment, targetView: ImageView, imageUrl: String) {
-        loadCircleImage(GlideApp.with(fragment), targetView, imageUrl)
-    }
-
-    // ScaleType 을 적용하면 blur 가 동작하지 않아, imageView 에 직접 scaleType  을 적용해야한다.
-    fun loadBlurImage(context: Context, targetView: ImageView, imageUrl: String) {
-        loadBlurImage(GlideApp.with(context), targetView, imageUrl)
-    }
-
-    fun loadBlurImage(activity: Activity, targetView: ImageView, imageUrl: String) {
-        loadBlurImage(GlideApp.with(activity), targetView, imageUrl)
-    }
-
-    fun loadBlurImage(fragment: Fragment, targetView: ImageView, imageUrl: String) {
-        loadBlurImage(GlideApp.with(fragment), targetView, imageUrl)
+    fun loadCircleImage(fragment: Fragment, targetView: ImageView, imageUrl: String, isNoCache: Boolean = false) {
+        loadCircleImage(GlideApp.with(fragment), targetView, imageUrl, isNoCache)
     }
 
     private fun loadImage(glide: GlideRequests, targetView: ImageView, imageUrl: String, isNoCache: Boolean) {
@@ -56,12 +42,33 @@ object ImageLoader {
         glideRequest.into(targetView)
     }
 
-    private fun loadCircleImage(glide: GlideRequests, targetView: ImageView, imageUrl: String) {
-        glide.load(imageUrl).apply(RequestOptions.circleCropTransform()).into(targetView)
+    private fun loadCircleImage(glide: GlideRequests, targetView: ImageView, imageUrl: String, isNoCache: Boolean) {
+        val glideRequest = glide.load(imageUrl)
+
+        if (isNoCache) {
+            glideRequest.diskCacheStrategy(DiskCacheStrategy.NONE).skipMemoryCache(true)
+        }
+        glideRequest.apply(RequestOptions.circleCropTransform()).into(targetView)
     }
 
-    private fun loadBlurImage(glide: GlideRequests, targetView: ImageView, imageUrl: String){
-        glide.load(imageUrl).apply(RequestOptions.bitmapTransform(BlurTransformation())).into(targetView)
-    }
+
+    /*
+   // ScaleType 을 적용하면 blur 가 동작하지 않아, imageView 에 직접 scaleType  을 적용해야한다.
+   fun loadBlurImage(context: Context, targetView: ImageView, imageUrl: String) {
+       loadBlurImage(GlideApp.with(context), targetView, imageUrl)
+   }
+
+   fun loadBlurImage(activity: Activity, targetView: ImageView, imageUrl: String) {
+       loadBlurImage(GlideApp.with(activity), targetView, imageUrl)
+   }
+
+   fun loadBlurImage(fragment: Fragment, targetView: ImageView, imageUrl: String) {
+       loadBlurImage(GlideApp.with(fragment), targetView, imageUrl)
+   }
+
+   private fun loadBlurImage(glide: GlideRequests, targetView: ImageView, imageUrl: String){
+       glide.load(imageUrl).apply(RequestOptions.bitmapTransform(BlurTransformation())).into(targetView)
+   }
+   */
 
 }
