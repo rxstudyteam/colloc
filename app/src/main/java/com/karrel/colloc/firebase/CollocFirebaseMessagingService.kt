698b883acda020fa7e459d640a864f715be4080e
rlawlgns077 <rlawlgns077@gmail.com>
2018-07-19 18:54:36 +0900

    1. postman에서 data만 보내야 background, foreground 일 때 onMessageReceived 에 들어오므로 data만 처리하도록 수정 2. API 26  Android O 이상 버전에서 푸시받기 위해 채널 생성

--- app/src/main/java/com/karrel/colloc/firebase/CollocFirebaseMessagingService.kt
+++ app/src/main/java/com/karrel/colloc/firebase/CollocFirebaseMessagingService.kt
@@ -1,9 +1,12 @@
 package com.karrel.colloc.firebase
 
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.media.RingtoneManager
 import android.support.v4.app.NotificationCompat
 import android.util.Log
@@ -30,17 +33,11 @@ class CollocFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d(TAG, "From: " + remoteMessage.from)
 
-        if(remoteMessage.notification != null) {
-            Log.d(TAG, "Notification Message Body: ${remoteMessage.notification?.body}")
-            Log.d(TAG, "Notification Message Data: ${remoteMessage.data}")
-            sendNotification(remoteMessage.notification?.body)
-        }
-
         if(remoteMessage.data != null) {
             Log.d(TAG, "Notification Message Data: ${remoteMessage.data}")
+            sendNotification(remoteMessage.data.toString())
         }
     }
-
 
     private fun sendNotification(body: String?) {
         val intent = Intent(this, MainActivity::class.java).apply {
@@ -48,18 +45,35 @@ class CollocFirebaseMessagingService : FirebaseMessagingService() {
             putExtra("Notification", body)
         }
 
+        val CHANNEL_ID = "CollocNotification"
+        val CHANNEL_NAME = "CollocChannel"
+        val description = "This is Colloc channel"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+
+        var notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if( android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance)
+            channel.description = description
+            channel.enableLights(true)
+            channel.lightColor = Color.RED
+            channel.enableVibration(true)
+            channel.setShowBadge(false)
+            notificationManager.createNotificationChannel(channel)
+        }
+
         var pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
         val notificationSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
 
-        var notificationBuilder = NotificationCompat.Builder(this,"Notification")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Push Notification FCM")
+        var notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setLargeIcon(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher))
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Colloc Notification")
                 .setContentText(body)
                 .setAutoCancel(true)
                 .setSound(notificationSound)
                 .setContentIntent(pendingIntent)
 
-        var notificationManager: NotificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.notify(0, notificationBuilder.build())
     }
 }
