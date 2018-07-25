9fbf2a101d54981063924c7295125e48d058730b
pluto <pluto@eastandroid.com>
2018-07-25 13:16:02 +0900

    add databinding

---
+++ app/src/main/java/com/karrel/colloc/main/CollocMainFr.kt
@@ -0,0 +1,13 @@
+package com.karrel.colloc.main
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class CollocMainFr : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+    }
+}
+
