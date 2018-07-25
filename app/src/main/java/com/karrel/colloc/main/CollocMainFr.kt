f15bd39e75ab39535d856fdcc5403f941ad26e60
pluto <pluto@eastandroid.com>
2018-07-25 23:16:33 +0900

    ui top

--- app/src/main/java/com/karrel/colloc/main/CollocMainFr.kt
+++ app/src/main/java/com/karrel/colloc/main/CollocMainFr.kt
@@ -1,17 +1,23 @@
 package com.karrel.colloc.main
 
+import android.annotation.SuppressLint
 import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
+import android.support.v4.widget.SwipeRefreshLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.karrel.colloc.R
 import com.karrel.colloc.databinding.CollocMainFrBinding
+import kotlinx.android.synthetic.main.colloc_main_fr.*
 
 
-class CollocMainFr : Fragment() {
+class CollocMainFr : Fragment(), SwipeRefreshLayout.OnRefreshListener {
+
+
     private lateinit var bb: CollocMainFrBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -21,13 +27,26 @@ class CollocMainFr : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        onLoadOnce()
         onLoad()
     }
 
-    private fun onLoad() {
+    private fun onLoadOnce() {
+        swipe_layer.setOnRefreshListener(this)
+        swipe_layer.setColorSchemeResources(android.R.color.holo_blue_dark,
+                android.R.color.holo_green_dark,
+                android.R.color.holo_orange_dark,
+                android.R.color.holo_red_dark)
         bb.vm = ViewModelProviders.of(this).get(CollocMainViewModel::class.java)
     }
 
+    private fun onLoad() {
 
+    }
+
+    //-------------------------------------------------------------------------------------
+    override fun onRefresh() {
+        Handler().postDelayed({ swipe_layer.isRefreshing = false }, 5000)
+    }
 }
 
--- app/src/main/java/com/karrel/colloc/main/CollocMainViewModel.kt
+++ app/src/main/java/com/karrel/colloc/main/CollocMainViewModel.kt
@@ -1,7 +1,6 @@
 package com.karrel.colloc.main
 
 import android.arch.lifecycle.ViewModel
-import android.app.Application
 
 class CollocMainViewModel : ViewModel() {
 
