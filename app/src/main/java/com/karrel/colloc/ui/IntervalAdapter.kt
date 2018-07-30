f4e1b55a27fbd38a6fd6e5abfef3a58f0bce6402
karrel <karrel84@naver.com>
2018-07-30 21:51:01 +0900

    기능 구현 : 지금 UI

---
+++ app/src/main/java/com/karrel/colloc/ui/IntervalAdapter.kt
@@ -0,0 +1,47 @@
+package com.karrel.colloc.ui
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.karrel.colloc.R
+import kotlinx.android.synthetic.main.item_interval.view.*
+import java.util.ArrayList
+
+class IntervalAdapter : RecyclerView.Adapter<IntervalAdapter.CurrentViewHolder>() {
+
+    private val itemList: ArrayList<IntervalItem> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrentViewHolder {
+        return CurrentViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_interval, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: CurrentViewHolder, position: Int) {
+        holder.setData(itemList[position])
+    }
+
+    fun addItem(intervalItem: IntervalAdapter.IntervalItem) {
+        itemList.add(intervalItem)
+        notifyDataSetChanged()
+    }
+
+    fun initItem() {
+        itemList.clear()
+    }
+
+
+    class CurrentViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        fun setData(item: IntervalAdapter.IntervalItem) {
+            itemView.time.text = item.time
+            itemView.emoticon.setImageResource(item.emoticon)
+            itemView.status.text = item.status
+        }
+    }
+
+
+    class IntervalItem(val time: String = "오후8시", val emoticon: Int = R.drawable.smile, val status: String = "좋음")
+}
---
+++ app/src/main/java/com/karrel/colloc/ui/IntervalFragment.kt
@@ -0,0 +1,54 @@
+package com.karrel.colloc.ui
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.karrel.colloc.R
+import com.karrel.colloc.viewmodel.MainViewmodel
+import com.karrel.colloc.viewmodel.MainViewmodelImpl
+import kotlinx.android.synthetic.main.fragment_current.*
+
+
+class IntervalFragment : Fragment() {
+
+    val viewModel: MainViewmodel = MainViewmodelImpl
+    private lateinit var adapter: IntervalAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_current, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+        setupButtonsEvents()
+        setupTestCurrentData()
+    }
+
+    private fun setupTestCurrentData() {
+        adapter.initItem()
+        adapter.addItem(IntervalAdapter.IntervalItem("오후 8시", R.drawable.smile, "좋음"))
+        adapter.addItem(IntervalAdapter.IntervalItem("오후 9시", R.drawable.smile, "좋음"))
+        adapter.addItem(IntervalAdapter.IntervalItem("오후 10시", R.drawable.smile, "좋음"))
+        adapter.addItem(IntervalAdapter.IntervalItem("오후 11시", R.drawable.smile, "좋음"))
+        adapter.addItem(IntervalAdapter.IntervalItem("오후 12시", R.drawable.smile, "좋음"))
+        adapter.addItem(IntervalAdapter.IntervalItem("오전 1시", R.drawable.smile, "좋음"))
+        adapter.addItem(IntervalAdapter.IntervalItem("오전 2시", R.drawable.smile, "좋음"))
+        adapter.addItem(IntervalAdapter.IntervalItem("오전 3시", R.drawable.smile, "좋음"))
+    }
+
+    private fun setupRecyclerView() {
+        recyclerView.layoutManager = LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
+        adapter = IntervalAdapter()
+        recyclerView.adapter = adapter
+    }
+
+    private fun setupButtonsEvents() {
+    }
+
+}
